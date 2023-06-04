135012db7f3aace225dc4c5a2ab39bec9122d6ee
tjahan805 <110358481+tjahan805@users.noreply.github.com>
2023-06-04 01:10:44 -0400

    Update Reminder.java

--- Reminder.java
+++ Reminder.java
@@ -1,30 +1,30 @@
 import java.util.Date;
 
 public class Reminder {
-    private String title;
-    private String description;
-    private Date date;
+  private String title;
+  private String description;
+  private Date date;
 
-    public Reminder(String title, String description, Date date) {
-        this.title = title;
-        this.description = description;
-        this.date = date;
-    }
+  public Reminder(String title, String description, Date date) {
+    this.title = title;
+    this.description = description;
+    this.date = date;
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public Date getDate() {
-        return date;
-    }
+  public Date getDate() {
+    return date;
+  }
 
-    @Override
-    public String toString() {
-        return title;
-    }
+  @Override
+  public String toString() {
+    return title;
+  }
 }
